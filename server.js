d09122f4994c44a936662238da5afba5a642586b
Daniel Sanchez <morkendi@gmail.com>
2023-05-28 13:51:46 -0600

    Create & Delete functions

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+// Require dependencies
 const PORT = process.env.PORT || 3001;
 const fs = require('fs');
-
+const path = require('path');
 
 // Require and assign express
 const express = require('express');
@@ -15,39 +16,44 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 // GET Requests
-app.get('api/notes', (req, res) => {
-    res.json(notesDB.slice(1))
-});
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/db/db.json'))
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-// TODO: Create Note function
-// TODO: Define function arguments
-function createNote () {
-
-}
-
-
+// Create Note function
 app.post('/api/notes', (req, res) => {
-    const newNote = createNote(req.body, notesDB);
-    res.json(newNote);
-})
+    let newNote = req.body;
+    let noteArray = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+    let noteLength = (noteArray.length).toString();
 
-// TODO: Delete Note function
-// TODO: Define function arguments
-function deleteNote () {
+    newNote.id = noteLength;
+    noteArray.push(newNote);
 
-}
+    fs.writeFileSync('./db/db.json', JSON.stringify(noteArray));
+    res.json(noteArray)
+});
+
+// Delete Note function
+app.delete('/api/notes/:id', (req, res) => {
+    let noteArray = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+    let noteID = (req.params.id).toString();
+
+
+    noteArray = noteArray.filter(currNote => {
+        return currNote.id != noteID;
+    })
+
+    fs.writeFileSync('./db/db.json', JSON.stringify(noteArray));
+    res.json(noteArray);
+});
 
 app.listen(PORT, () => {
     console.log(`Listening to server on PORT ${PORT}`)
